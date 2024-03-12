d86949c6b37fc1f945a93abf96a99fbaa7f0c39f
Drennix Guerrero <61646218+devangerine@users.noreply.github.com>
2024-03-12 23:58:29 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -6,9 +6,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
 
+    string itemList = "\n 1.Rapi  \n 2.Anis  \n 3.Neon \n  4.Marian";
+
     constructor() ERC20("Degen", "DGN") {}
 
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
     }
+
+    function decimals() override public pure returns(uint8){
+        return 0;
+    }
+
+    function displayShopItems() public view returns (string memory){
+        return itemList;
+    }
+
+   // function buyFigure(uint256 _itemNumber) public 
+
+   // create a global variable to store itemInventory of user
+
+   // import burnable 
+
 }
