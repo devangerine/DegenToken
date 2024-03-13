125ad0198061e4068e8e3bc414520917159315ac
Drennix Guerrero <61646218+devangerine@users.noreply.github.com>
2024-03-13 18:43:12 +0800

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
 
     //Redeemable Items List 
-    string itemList = "\n 1.Rapi  \n 2.Anis  \n 3.Neon \n  4.Marian";
+    string itemList = "All Items are 100 DGN each: \n 1.Rapi  \n 2.Anis  \n 3.Neon \n  4.Marian";
     uint256 redeemPrice = 100;
 
     constructor() ERC20("Degen", "DGN") {}
@@ -32,9 +32,12 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
     mapping(address => uint256[]) private userInventory;
 
     //Function to redeem items from the shop
-    function redeemItem(uint256 _itemNumber) public {
+    function redeemItem(uint256 _itemNumber) external{
         require(balanceOf(_msgSender())>= redeemPrice, "Your balance of Degen tokens is insufficient.");
         updateInventory(_msgSender(), _itemNumber);
+        approve(_msgSender(), redeemPrice);
+        burn(redeemPrice);
+        
     }
 
     //Function to update the inventory of users upon redemption of items using tokens
@@ -43,7 +46,7 @@ contract DegenToken is ERC20, ERC20Burnable, Ownable {
     }
 
     //Function to display the inventory of the user
-    function displayInventory() public view returns (uint256[] memory){
+    function displayInventory() external view returns (uint256[] memory){
         return userInventory[_msgSender()];
     }
 
